78573c4bec5c00d32f29206f2acf4e4e7ca22c01
Yori Zhao <zjkhiyori@gmail.com>
2019-04-25 16:24:40 +0800

    [bugfix]: change resource name

--- app/src/main/java/club/syachiku/svganimator/MainActivity.java
+++ app/src/main/java/club/syachiku/svganimator/MainActivity.java
@@ -8,8 +8,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,25 +20,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         final ImageView imageView = findViewById(R.id.imgView);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                     if (checked) {
-                        imageView.setImageDrawable(getDrawable(R.drawable.back));
+                        imageView.setImageDrawable(getDrawable(R.drawable.ani_back));
                         checked = false;
                     } else {
-                        imageView.setImageDrawable(getDrawable(R.drawable.menu));
+                        imageView.setImageDrawable(getDrawable(R.drawable.ani_menu));
                         checked = true;
                     }
                     ((Animatable) imageView.getDrawable()).start();
